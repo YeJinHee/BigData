a761f3f47c3cbb39017972137bc1057ef7954099
sora <soraluffy26@gmail.com>
2023-10-19 04:00:07 +0900

    commit 0.7v

--- week02_numpy.py
+++ week02_numpy.py
@@ -1,24 +1,30 @@
 import numpy as np
 import random
+import tkinter as tk  # Built in GUI
 
-#v = np.array([1,3,-9,2])
-#v = np.array([
-   # [1,3,-9,2],
-    #[71,13,-22,9]] , dtype='int64')
+def click_button():
+    n = int(en_number.get())
+    l = [random.randint(1, 100) for i in range(n)]
+    v = np.array(l, dtype='int16')
+    lbl_result.config(text=v)
 
-#v = np.array([
- #   [1,3,-9,2],
-  #  [71,13,-22,9]] )#, dtype='int64')
 
-#n = int(input("input number: "))
-#l = [random.randint(a: 1, b:100) for i in range(n)]
-#v = np.array(l, dtype='int16')
+window = tk.Tk()
+window.title('numpy gui version v0.7')
+window.geometry('300x150')
 
-#print(v)
+# create widget
+lbl_result = tk.Label(text="random numpy array")
+en_number = tk.Entry()
+btn_click = tk.Button(text="click me!", command=click_button)
 
-n = int(input("input number : "))
-l = [random.randint(1, 100) for i in range(n)]
-v = np.array(l, dtype='int16')
-print(v)
+# widget layout
+lbl_result.pack()
+en_number.pack(fill='x')
+btn_click.pack(fill='x')
 
-print(v.ndim, v.shape, v.data, v.dtype, v.strides)
+window.mainloop()
+# n = int(input("input number : "))
+# l = [random.randint(1, 100) for i in range(n)]
+# v = np.array(l, dtype='int16')
+# print(v)
--- week07_web02.py
+++ week07_web02.py
@@ -9,7 +9,7 @@ cities = soup.find_all("city")
 data = soup.find_all("data")
 dates = soup.find("tmef") 
 
-print(dates.string) #처음 일기예보가 초기화되었을 때 날짜 데이터
+print(dates.string) # 처음 일기예보가 초기화되었을 때 날짜 데이터
 for i in range(len(cities)):
     print(f'{cities[i].string}의 중기 예보 첫 번째 날씨는 {data[i*13].find("wf").string}입니다.')
 
