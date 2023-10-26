20e7190389389e279a393915b2a196858854c302
sora <soraluffy26@gmail.com>
2023-10-26 16:28:36 +0900

    week09 v04

--- week09_numpy_review.py
+++ week09_numpy_review.py
@@ -1,14 +1,6 @@
 import numpy as np
-data = np.array([
-    [1, 5, 9],
-    [2, np.nan, 10],
-    [np.nan, 7, 11],
-    [4, 8, np.nan]
-])
-print(data)
-
-means = np.nanmean(data, axis=0)  # 각 column의 평균 값들
-for i in range(data.shape[1]):
-    mask = np.isnan(data[:, i]) # True, False 값을 갖는 배열 생성
-    data[mask, i] = means[i]
-print(data)
+a1 = np.array([1,2,3])
+a2 = np.array([4,5,6])
+print(a1*a2)
+a3=np.dot(a1,a2)
+print(a3)
