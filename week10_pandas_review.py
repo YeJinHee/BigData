f98456778e1f527994110724f8682eb768c9a116
sora <soraluffy26@gmail.com>
2023-11-02 15:22:37 +0900

    week10 v0.2

--- week10_pandas_review.py
+++ week10_pandas_review.py
@@ -1,13 +1,11 @@
 import numpy as np
-a1 = np.array([
-    [1,2],
-    [1,2]
-])
+a1 = np.ones((3, 3))
+a1[:, 2] = 2
+print(a1)
 a2 = np.array([
-    [2, 1],
-    [5, 0]
+    [1, 0, 0],
+    [2, 0, 0],
+    [1, 0, 0],
 ])
-
-#print(np.dot(a1, a2))
-#print(a1@a2)
-print(np.matmul(a1, a2))
+print(a2)
+print(np.matmul(a1, a2))  # 행렬 곱
