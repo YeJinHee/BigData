a05e205a779399d8e60441f6d6767b9a820434f2
sora <soraluffy26@gmail.com>
2023-11-02 16:31:08 +0900

    week10 v0.7

--- week10_pandas_review.py
+++ week10_pandas_review.py
@@ -1,23 +1,11 @@
 import numpy as np
-a1 = np.arange(1, 6)
-a2 = np.array([6, 7, 8, 9, 10])
-a3 = np.array([
-    [3, 2, 1],
-    [-11, 0, 9]
-])
-print(a1 + a2)
-print(a1 - a2)
-print(a1 * a2)
-print(a1 / a2)
-print(np.dot(a1, a2))
-# a3 = a1 + a2
-# print(a3)
-print(np.sum(a1) + np.sum(a2))
-print(np.prod(a1), np.prod(a2))
-# by axis
-print(a3.sum(axis=0))
-print(a3.sum(axis=1))
-print(a3.prod(axis=0))
-print(a3.prod(axis=1))
-print(a3.mean())  # 산술평균
-print(a3.std())  # 표준편차
+import matplotlib.pyplot as plt
+
+
+np.random.seed()
+a = np.random.uniform(size=10).reshape(2, 5) #균등분포
+b = np.random.normal(0, 1, 100) #정규분포
+print(a)
+print(b)
+plt.hist(b)
+plt.show()
