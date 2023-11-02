bf5b49de5d50f79a7fd0e452c6c75bd5afe02345
sora <soraluffy26@gmail.com>
2023-11-02 16:33:29 +0900

    week10 v0.8

--- week10_pandas_review.py
+++ week10_pandas_review.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 np.random.seed()
-a = np.random.uniform(size=10).reshape(2, 5) #균등분포
-b = np.random.normal(0, 1, 100) #정규분포
+#a = np.random.uniform(size=10).reshape(2, 5) #균등분포
+a = np.random.uniform(size=10000)
+b = np.random.normal(50, 1, 100) #정규분포
 print(a)
 print(b)
 plt.hist(b)
