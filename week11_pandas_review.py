c593a1101e706b111754dfd5369e78a391645d5c
sora <soraluffy26@gmail.com>
2023-11-09 15:22:13 +0900

    week11 v0.5

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -15,11 +15,19 @@ print(df1)
 df2 = pd.DataFrame(
     [[99, 89, 100],
         [91, 98, 90],
-        [100, 97, 85]],
-    index=[1, 2, 3],
+        [100, 97, 85],
+        [83, 100, 85]],
+    index=[1, 2, 3, 4],
     columns=['KOR', 'ENG', 'MAT']
 )
 print(df2)
+#df2_copy = df2.iloc[:, [0, 2]] #국어 수학만 출력
+#df2_copy = df2.loc[:, ['KOR', 'MAT']] #얘도 똑같음
+#df2_copy = df2.loc[:, 'KOR': 'MAT'] #영어 안 지워짐
+df2_copy = df2.loc[:, 'KOR': 'MAT' :2] #지워짐
+print(df2_copy)
+
+
 df2 = (pd.melt(df2)
        .rename(columns={
         'variable': 'subject',
@@ -29,4 +37,4 @@ df2 = (pd.melt(df2)
        )
 
 print(df2)
-
+print(df2.iloc[:, [1]]) #전체에서 1행만 출력
