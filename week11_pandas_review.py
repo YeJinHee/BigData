5e2bf57f58d03518a619200cb691000ddffbce63
sora <soraluffy26@gmail.com>
2023-11-09 16:31:10 +0900

    week11 v1.1

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -1,20 +1,30 @@
-import pandas as pd
+#pandas 사용 안 하고 짠 코드
 
-df = pd.DataFrame(
-    [[99, 89, 100],
+scores = [[99, 89, 81],
         [91, 98, 90],
         [95, 97, 85],
-        [83, 100, 94]],
-    index=[1, 2, 3, 4],
-    columns=['KOR', 'ENG', 'MAT']
-)
-print(df)
+        [83, 96, 94]]
 
-df = df.apply(lambda n: n+1) #얘도 1점 추가함
-print(df)
+index = [1, 2, 3, 4]
+columns = ['KOR', 'ENG', 'MAT']
 
-# def scale_score(n): #점수 조정 1점씩 추가
-#     return n + 1
-#
-# df = df.apply(scale_score)
-# print(df)
+for c in columns:
+    print(f'{c}', end=' ')
+print()
+for row in range(4):
+    print(index[row], end=' ')
+    for col in range(3):
+        print(scores[row][col], end=' ')
+        scores[row][col] = scores[row][col]+1  # update
+    print()
+
+print()
+
+for c in columns:
+    print(f'{c}', end=' ')
+print()
+for row in range(4):
+    print(index[row], end=' ')
+    for col in range(3):
+        print(scores[row][col], end=' ')
+    print()
