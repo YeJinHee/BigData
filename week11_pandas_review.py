2d7c2a5109082521e94933c6271b112399830388
sora <soraluffy26@gmail.com>
2023-11-09 14:24:12 +0900

    week11 v0.1

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -2,19 +2,18 @@ import pandas as pd
 
 df1 = pd.DataFrame(
     {
-        "KOR" : [99, 91, 100],
+        "KOR": [99, 91, 100],
         "ENG": [89, 98, 97],
-        "MAT": [100, 90, 85]
-    }, index = [1, 2, 3]
+        "MAT": [100, 90, 85],
+    }, index=[1, 2, 3]
 )
-
 print(df1)
 
 df2 = pd.DataFrame(
-    [
-        [99, 89, 100]
-    ], index = [1, 2, 3],
-    columns=["KOR", "ENG", "MAT"]
+    [[99, 89, 100],
+        [91, 98, 90],
+        [100, 97, 85]],
+    index=[1, 2, 3],
+    columns=['KOR', 'ENG', 'MAT']
 )
-
 print(df2)
