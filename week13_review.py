80be3973593e93c22549ed91b6413291b27a48c5
sora <soraluffy26@gmail.com>
2023-11-23 16:33:31 +0900

    week13 v0.3

--- week13_review.py
+++ week13_review.py
@@ -4,9 +4,14 @@ import pandas as pd
 titanic = sns.load_dataset('titanic')
 
 print(titanic.isnull().sum())
+print(titanic['age'].fillna(titanic['age'].median(), inplace=True))
+print(titanic.isnull().sum())
+# titanic['deck'].fillna('Unknown', inplace=True)  # 범주형 카테고리에 Unknown이 없음
+titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')  # 카테고리 추가
+titanic['deck'].fillna('Unknown', inplace=True)
 
-#print(titanic.head())
-#print(titanic.describe())
-#print(titanic.info())
-#print(titanic.tail())
-#print(titanic())
+# print(type(titanic))
+# print(titanic.describe())
+# print(titanic.info())
+# print(titanic.tail())
+print(titanic['deck'].tail(10))
