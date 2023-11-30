6f78feef94755d2cc0a837623e864fab8d6a6052
sora <soraluffy26@gmail.com>
2023-11-30 15:04:01 +0900

    week14 v0.2

--- week14_dataset.py
+++ week14_dataset.py
@@ -1,10 +1,16 @@
 import seaborn as sns
 
 titanic = sns.load_dataset('titanic')
-#print(titanic.info())
+# print(titanic.info())
+# print(titanic.head())
 
 #남성과 여성의 생존률
-
+male_survived = titanic[(titanic['survived'] == 1) & (titanic['sex'] == 'male')]['survived'].count()
+female_survived = titanic[(titanic['survived'] == 1) & (titanic['sex'] == 'female')]['survived'].count()
+male_count = titanic[(titanic['sex'] == 'male')]['sex'].count()
+female_count = titanic[(titanic['sex'] == 'female')]['sex'].count()
+print(male_survived, male_count, female_survived, female_count)
+print(male_survived/male_count, female_survived/female_count)
 
 #생존자와 사망자의 수를 카운드
 #판다스 사용?
