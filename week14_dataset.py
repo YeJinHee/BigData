a010784a8b6503a75b5705fa4fd4cb7034fe115f
sora <soraluffy26@gmail.com>
2023-11-30 14:42:25 +0900

    week14 v0.1

--- week14_dataset.py
+++ week14_dataset.py
@@ -1,12 +1,14 @@
 import seaborn as sns
-import pandas as pd
 
 titanic = sns.load_dataset('titanic')
-#print(titanic.describe())
+#print(titanic.info())
 
-print(titanic['age'].fillna(titanic['age'].median(), inplace=True))
-titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')  # 카테고리 추가
-titanic['deck'].fillna('Unknown', inplace=True)
-titanic['embarked'].fillna('Unknown', inplace=True)
-titanic['embark_town'].fillna('Unknown', inplace=True)
-print(titanic.info(10))
+#남성과 여성의 생존률
+
+
+#생존자와 사망자의 수를 카운드
+#판다스 사용?
+# survived_human = titanic[titanic["survived"] == 1]["survived"].count() #살아있는 사람의 수 카운트
+# dead_human = titanic[titanic["survived"] == 0]["survived"].count() #사망한 사람의 수 카운트
+# print(f"생존자 수 : {survived_human}")
+# print(f"사망자 수 : {dead_human}")
