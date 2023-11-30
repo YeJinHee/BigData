6ef207bb390615557c39eb38f2955e350bc2fc4c
sora <soraluffy26@gmail.com>
2023-11-30 15:26:08 +0900

    week14 v0.4

--- week14_dataset.py
+++ week14_dataset.py
@@ -1,8 +1,15 @@
 import seaborn as sns
+import pandas as pd
 
 titanic = sns.load_dataset('titanic')
 # print(titanic.info())
 # print(titanic.head())
+
+# 나이대별 생존자 수
+# 데이터를 나이대로 잘라서 사용할 것이기 떄문에 판다스의 qcut 사용
+# [0, 10, 20, 30, 40, 50, 60 , 70, 80]
+age_survived = titanic.groupby(pd.cut(titanic['age'], bins=range(0, 81, 10)))['survived'].sum()
+print(age_survived)
 
 # 객실 등급별 생존자 수
 # 여기서도 판다스 예제 사용 그룹바이?
@@ -11,8 +18,8 @@ titanic = sns.load_dataset('titanic')
 #pclass_survived = titanic.groupby('pclass')['survived'].sum()
 
 #다른 학생 코드
-pclass_survived = titanic[titanic['survived']==1].groupby(['pclass']).size()
-print(pclass_survived)
+# pclass_survived = titanic[titanic['survived']==1].groupby(['pclass']).size()
+# print(pclass_survived)
 
 
 #남성과 여성의 생존률
